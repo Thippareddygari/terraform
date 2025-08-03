c3dccc85774b17024623acc927fb1082cacb718a
kishor <kishorebsc12@gmail.com>
2025-08-03 12:45:39 +0530

    added

--- frontend.tf
+++ frontend.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t2.micro"
-
+    vpc_security_group_ids = ["sg-0930eb7aaaf6a5468"]
     tags = {
       Name = "frontend"
     }
