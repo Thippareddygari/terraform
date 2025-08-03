cd770d485f9086c7398c980ab591156ae775be7f
kishor <kishorebsc12@gmail.com>
2025-08-03 17:26:35 +0530

    added

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "dummy" {
     count = length((var.x))
 
     provisioner "local-exec" {
-      command = "echo ${var.x}[0]"
+      command = "echo ${var.x}[count.index]"
     }
 }
 
