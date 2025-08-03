7ec5ea127a26e08cb6eb347914777cb17a6bcad8
kishor <kishorebsc12@gmail.com>
2025-08-03 16:44:33 +0530

    added

--- frontend.tf
+++ frontend.tf
@@ -1,34 +1,32 @@
 resource "aws_instance" "frontend" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-0930eb7aaaf6a5468"]
-    tags = {
-      Name = "frontend"
-    }
-    
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0930eb7aaaf6a5468"]
+
+  tags = {
+    Name = "frontend"
+  }
 }
 
-
 resource "aws_route53_record" "frontend" {
-  name="frontend-dev"
   zone_id = "Z10310253KPZLFJOC7YEK"
   type = "A"
+  name = "frontend-dev"
   ttl = 300
   records = [aws_instance.frontend.private_ip]
 }
 
 resource "null_resource" "frontend" {
   provisioner "remote-exec" {
-        connection {
-            type = "ssh"
-            user = "ec2-user"
-            password = "DevOps321"
-            host = aws_instance.frontend.private_ip
-      
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = aws_instance.frontend.private_ip
     }
-        inline = [
-            "pip3.11 install ansible",
-            "ansible-pull -i localhost, -U  https://github.com/Thippareddygari/ansible-shop shop.yml -e component_name=frontend -e env=dev",
-        ]
+    inline = [
+      "pip3.11 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/Thippareddygari/ansible-shop shop.yml -e component_name=frontend -e env=dev",
+    ]
   }
 }
