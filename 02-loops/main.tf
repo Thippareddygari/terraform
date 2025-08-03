df8b51d7fb6b7f3fdea87a3397f72545df5f6c73
kishor <kishorebsc12@gmail.com>
2025-08-03 17:24:24 +0530

    added

--- 02-loops/main.tf
+++ 02-loops/main.tf
@@ -1,6 +1,12 @@
 resource "null_resource" "dummy" {
     count = length((var.x))
+
+    provisioner "local-exec" {
+      command = "echo ${var.x}"
+    }
 }
+
+
 
 variable "x" {
   default = [1,200,300,400]
